7a37f079d403a89537049bab769ffa1a5c7c4683
mahdialikhani2005-maker <mahdialikhani2005@gmail.com>
2026-09-20 18:22:27 +0330

    m

--- back/backend/app/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from .user import User
-from .heart import Heart
-from .progress import Progress
-from .premium import Premium
-from .subscription import Subscription
-from .lesson import Lesson
-from .word import Word, LearnedWord
